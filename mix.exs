a9136f2b3cc02074cf24e1635ac0d94d8fa7d322
Caique Rodrigues <caiquepeixoto1@gmail.com>
2018-06-22 19:29:23 -0300

    Add Travis CI support with coverage report configured.

--- mix.exs
+++ mix.exs
@@ -10,7 +10,13 @@ defmodule SandboxCi.Mixfile do
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test,
+      ],
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -38,7 +44,8 @@ defmodule SandboxCi.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:excoveralls, "~> 0.9.1"}
     ]
   end
 
